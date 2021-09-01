d1e1882bbbb1638ea7b5e86c5fc2b701d2b1a709
BoraldPelmenier@gmail.com <58140350+BoraldPelmenier@users.noreply.github.com>
2021-09-02 00:43:19 +0700

    first commit

---
+++ UnitTestCsvReaderApp/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csvReaderApp;
+
+namespace UnitTestCsvReaderApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodLoadFile()
+        {
+            var items = UserItem.LoadFile(@"D:\\userItems.csv");
+            Assert.AreEqual(30, items.Count);
+        }
+    }
+}
---
+++ csvReaderApp/UserItem.cs
@@ -0,0 +1,59 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace csvReaderApp
+{
+    public class UserItem
+    {
+        public int Id { get; set; }
+        public string Region { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+
+        public static List<UserItem> LoadFile(string path)
+        {
+            List<UserItem> items = new List<UserItem>();
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ","
+            };
+
+            using (var reader = new StreamReader(path))
+            {
+                using (var csv = new CsvReader(reader, config))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+                    while (csv.Read())
+                    {
+                        var userItem = new UserItem
+                        {
+                            Id = Convert.ToInt32(csv.GetField("Id")),
+                            Region = csv.GetField("Region"),
+                            FirstName = csv.GetField("FirstName"),
+                            LastName = csv.GetField("LastName"),
+                            Phone = csv.GetField("Phone")
+                        };
+
+                        items.Add(userItem);
+                    }                    
+                }
+            }
+
+            return items;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}. Region: {Region} | {FirstName} {LastName} | phone: {Phone} .";
+        }
+    }
+}
